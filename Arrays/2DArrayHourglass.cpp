cd150e2430c8c439219f66a421e7c243294da830
Gouri S Nair <39928477+gourisnair@users.noreply.github.com>
2020-11-11 11:52:52 +0530

    Create 2DArrayHourglass.cpp

---
+++ Arrays/2DArrayHourglass.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the hourglassSum function below.
+int hourglassSum(vector<vector<int>> arr) {
+    int sum = 0, max = -63; //max is initialized to -63 bcos that's the least possible value for sum (-9*7 = -63 for an hourglass)
+    vector<int>hoursum(16);
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            sum = (arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+
+            arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]);
+            
+            if(sum > max) {
+                max = sum;
+            }
+            sum = 0;
+        }
+    }
+    return max;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    vector<vector<int>> arr(6);
+    for (int i = 0; i < 6; i++) {
+        arr[i].resize(6);
+
+        for (int j = 0; j < 6; j++) {
+            cin >> arr[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int result = hourglassSum(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
