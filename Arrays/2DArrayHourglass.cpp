b06b6f2cc96a4ac6a77f7853d4b8d21ffc585135
Gouri S Nair <39928477+gourisnair@users.noreply.github.com>
2020-11-12 09:03:36 +0530

    Update 2DArrayHourglass.cpp

--- Arrays/2DArrayHourglass.cpp
+++ Arrays/2DArrayHourglass.cpp
@@ -4,13 +4,10 @@ using namespace std;
 
 // Complete the hourglassSum function below.
 int hourglassSum(vector<vector<int>> arr) {
-    int sum = 0, max = -63; //max is initialized to -63 bcos that's the least possible value for sum (-9*7 = -63 for an hourglass)
-    vector<int>hoursum(16);
+    int sum = 0, max = -63; //max possible value for hourglass sum is -63(-9*7)
     for(int i = 0; i < 4; i++) {
         for(int j = 0; j < 4; j++) {
-            sum = (arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+
-            arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]);
-            
+            sum = (arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]);
             if(sum > max) {
                 max = sum;
             }
